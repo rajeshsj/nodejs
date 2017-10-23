f5eef2b4059bbf21925501930960bd0d172a8005
rajeshsj <32957821+rajeshsj@users.noreply.github.com>
2017-10-23 08:46:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
+// const https = require('https');
+ 
+ 
+//  exports.findBus = (req, res) => {
+//     const assistant = new Assistant({ request: req, response: res });
+
+//     function responseHandler(assistant) {
+//         var busNumber = assistant.getArgument('busnumber');
+//         var busDirection = assistant.getArgument('direction').toLowerCase();
+
+//      https.get('https://demo6927372.mockable.io/northbus', (resp) => {
+//   let data = '';
+ 
+//   // A chunk of data has been recieved.
+//   resp.on('data', (chunk) => {
+//     data += chunk;
+//   });
+ 
+//   // The whole response has been received. Print out the result.
+//   resp.on('end', () => {
+//     console.log(JSON.parse(data).northDirectionAvailableBus);
+//   });
+ 
+// }).on("error", (err) => {
+//   console.log("Error: " + err.message);
+// });
+//  }
+// 	}
 const https = require('https');
  
- 
- exports.findBus = (req, res) => {
-    const assistant = new Assistant({ request: req, response: res });
-
-    function responseHandler(assistant) {
-        var busNumber = assistant.getArgument('busnumber');
-        var busDirection = assistant.getArgument('direction').toLowerCase();
-
-     https.get('https://demo6927372.mockable.io/northbus', (resp) => {
+https.get('https://demo6927372.mockable.io/northbus', (resp) => {
   let data = '';
  
   // A chunk of data has been recieved.
@@ -24,5 +44,4 @@ const https = require('https');
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
- }
-	}
+
