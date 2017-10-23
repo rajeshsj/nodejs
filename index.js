d2d777bea6b57539434da93b8af7f1b04a549c3f
rajeshsj <32957821+rajeshsj@users.noreply.github.com>
2017-10-23 15:04:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,22 +26,18 @@
 // });
 //  }
 // 	}
+// });
 const https = require('https');
- 
-https.get('https://demo6927372.mockable.io/northbus', (resp) => {
-  let data = '';
- 
-  // A chunk of data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
- 
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log(JSON.parse(data).northDirectionAvailableBus);
-  });
- 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
+exports.dialogflowFirebaseFulfillment = function dialogflowFirebaseFulfillment (req, res) {
+  //response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+https.get('https://demo6927372.mockable.io/northbus', (res) => {
+	let data = '';
+   res.on('data', (chunk) => {
+     data += chunk;
+   });});
+  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({ "speech": response, "displayText": response 
+  //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
+};
 
